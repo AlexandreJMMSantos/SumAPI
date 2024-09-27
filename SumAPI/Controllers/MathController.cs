4ffa882c575b444d98f023d151cba8512786bcf1
AlexandreJMMSantos <alexandretcq@gmail.com>
2024-09-27 15:41:30 -0300

    Add files via upload

---
+++ SumAPI/Controllers/MathController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SumAPI.Controllers
+{
+    public class MathController : Controller
+    {
+        [HttpGet("RealizaSoma")]
+        public IActionResult Sum([FromQuery] int v1, [FromQuery] int v2)
+        {
+            int resultado = v1 + v2;
+
+            return Ok(resultado);
+        }
+    }
+}
